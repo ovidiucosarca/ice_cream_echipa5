8edb9e779310dda5b416939cbb587e8ab456089d
ovidiucosarca <122724900+ovidiucosarca@users.noreply.github.com>
2023-04-11 12:51:44 +0000

    Deploying to gh-pages from @ ovidiucosarca/ice_cream_echipa5@d1fdfab51aca113922d9caa0f6be07f1f553bb80 🚀

--- index.8d2a6ee4.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.8d2a6ee4.js.map
---
+++ index.bccedfea.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:1200,parallax:!0,autoplay:{delay:3500,disableOnInteraction:!0},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.bccedfea.js.map
